4b1d2ab1a49682fa7ae6fb2e7d6e933eaa40ea63
Lanz Hilario <85623845+lazn327@users.noreply.github.com>
2025-05-20 14:01:03 +0800

    Add files via upload
    Updates:
    - Modified javascript structure
    - CSS properties replaced with the variables

--- login.js
+++ login.js
@@ -1,44 +1,38 @@
-const darkmodeBtn = document.getElementById("darkmode");
-const lightmodeBtn = document.getElementById("lightmode");
 
-const input = document.querySelector(".loginContainer input");
-const passInput = document.querySelector("#passInput");
+// for darkmode/lightmode
+let darkmode = localStorage.getItem('darkmode');
 
-darkmodeBtn.addEventListener('click', function(){
-    input.classList.toggle("dark-placeholder");
-    input.style.color = "white";
-    passInput.classList.toggle("dark-placeholder");
-    passInput.style.color = "white";
+const themeSwitch = document.getElementById("theme-switch");
 
-    document.getElementById("resetDiv").style.background = "rgba(73, 73, 73, 0.45)";
-    document.getElementById("resetTitle").style.color = "white";
-    document.getElementById("resetText").style.color = "white";
+const enableDarkmode = () => {
+  document.body.classList.add('darkmode');
+  localStorage.setItem('darkmode', 'active');
+}
 
-    document.body.style.backgroundImage = "url('loginDocs/image\ 11.png')";
-    darkmodeBtn.style.display = "none";
-    lightmodeBtn.style.display = "block";
-    document.getElementById("title").style.color = "gold";
-    document.getElementById("subTitle").style.color = "white";
-    document.getElementById("tennets").style.color = "gold";
-    document.getElementById("copyright").style.color = "white";
-    document.getElementById("loginTitle").style.color = "white";
-    document.getElementById("loginDiv").style.background = "rgba(73, 73, 73, 0.45)";
-    document.getElementById("loginBtn").style.backgroundColor = "black";
-    document.getElementById("loginBtn").style.color = "white";
-    document.getElementById("resetBtn").style.color = "white";
-    document.getElementById("loginBtn").style.border = "1px solid white";
-    document.getElementsByClassName("inputDetail").style.background = "rgba(184, 184, 184, 0.3)";
+const disableDarkmode = () => {
+  document.body.classList.remove('darkmode');
+  localStorage.setItem('darkmode', 'inactive');
+}
 
-});
+if(darkmode === "active") enableDarkmode();
 
-lightmodeBtn.addEventListener('click', function(){
-    location.reload();
+themeSwitch.addEventListener('click', function(){
+  darkmode = localStorage.getItem('darkmode');
+
+  if(darkmode !== "active"){
+    enableDarkmode();
+  }
+  else{
+    disableDarkmode();
+  }  
 });
 
 
-const resetPassBtn = document.getElementById("resetBtn");
+// Reset Confirmation Page
 
-resetPassBtn.addEventListener('click', function(){
-    document.getElementById("resetDiv").style.display = "block";
-    document.getElementById("loginDiv").style.display = "none";
+const resetBtn = document.getElementById("resetBtn");
+
+resetBtn.addEventListener('click', function(){
+  document.getElementById("loginDiv").style.display = "none";
+  document.getElementById("resetDiv").style.display = "block";
 });
