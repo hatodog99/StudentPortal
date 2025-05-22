f769621f2d18e0e1f703b448ba486027af802ae0
pol <paulpabahay@gmail.com>
2025-05-22 09:04:38 +0800

    Add files via upload
    adjsutments para maging kamuka nung sa figma file
    
    in progress:
    making it responsive

--- login.js
+++ login.js
@@ -1,38 +1,8 @@
-
-// for darkmode/lightmode
-let darkmode = localStorage.getItem('darkmode');
-
-const themeSwitch = document.getElementById("theme-switch");
-
-const enableDarkmode = () => {
-  document.body.classList.add('darkmode');
-  localStorage.setItem('darkmode', 'active');
-}
-
-const disableDarkmode = () => {
-  document.body.classList.remove('darkmode');
-  localStorage.setItem('darkmode', 'inactive');
-}
-
-if(darkmode === "active") enableDarkmode();
-
-themeSwitch.addEventListener('click', function(){
-  darkmode = localStorage.getItem('darkmode');
-
-  if(darkmode !== "active"){
-    enableDarkmode();
-  }
-  else{
-    disableDarkmode();
-  }  
-});
-
-
 // Reset Confirmation Page
 
 const resetBtn = document.getElementById("resetBtn");
 
-resetBtn.addEventListener('click', function(){
+resetBtn.addEventListener("click", function () {
   document.getElementById("loginDiv").style.display = "none";
   document.getElementById("resetDiv").style.display = "block";
 });
