ed720a878c4fb585418509a5c6434896b3b63af3
pol <paulpabahay@gmail.com>
2025-06-10 01:44:39 +0800

    DONEEEEEEE AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA
    - all features
    - animations

--- calendar-page/calendar.js
+++ calendar-page/calendar.js
@@ -3,19 +3,10 @@ document.addEventListener("DOMContentLoaded", function () {
   const extendIcon1 = document.getElementById("extend-icon-1");
   const extendIcon2 = document.getElementById("extend-icon-2");
 
+  const mainCalContainer = document.getElementById("main-cal-container");
   const extendedContainer = document.getElementById("extended-container");
 
   extendBtn.addEventListener("click", function () {
-    if (extendedContainer.style.display === "none") {
-      extendedContainer.style.display = "flex";
-
-      extendIcon1.style.display = "block";
-      extendIcon2.style.display = "none";
-    } else {
-      extendedContainer.style.display = "none";
-
-      extendIcon1.style.display = "none";
-      extendIcon2.style.display = "block";
-    }
+    extendedContainer.classList.toggle("hidden");
   });
 });
